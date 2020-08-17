0047a5f1fb0669812e6fad0393611964ea7558d0
YuudachiXMMY <934647325@qq.com>
2020-08-17 02:16:40 -0700

    add detailed schedule. ready to rebuild the auto scroll api

--- public_html/gj/main/index.js
+++ public_html/gj/main/index.js
@@ -12,12 +12,11 @@
   window.addEventListener("load", init);
 
   function init() {
-      var myFullpage = new fullpage('#fullpage', {
-        lazyLoad: true,
-        verticalCentered: false,
+    var myFullpage = new fullpage('#fullpage', {
+      verticalCentered: false,
 
-        //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
-        css3:false
+      //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
+      css3:false
     });
   }
 
