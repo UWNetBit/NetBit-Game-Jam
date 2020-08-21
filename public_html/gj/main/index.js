8682c9cb6d491ff9e568987f03a5df9b9fa2d985
YuudachiXMMY <934647325@qq.com>
2020-08-20 18:19:05 -0700

    Basical Developed

--- public_html/gj/main/index.js
+++ public_html/gj/main/index.js
@@ -14,10 +14,11 @@
   function init() {
     var myFullpage = new fullpage('#fullpage', {
       verticalCentered: false,
-
       //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
       css3:false
     });
+
+    
   }
 
 
